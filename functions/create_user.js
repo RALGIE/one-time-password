f211510ef23d0e1687e050fda80ed2dbd40f4db7
Ralph Gielkens <ralph.gielkens@bct.nl>
2018-08-18 22:45:53 +0200

    Section 6 Collage 55

--- functions/create_user.js
+++ functions/create_user.js
@@ -1,3 +1,18 @@
+const admin = require('firebase-admin')
+
 module.exports = function(req, res){
-  res.send(req.body);
+  //Verify the user provide a phone
+  if (!req.body.phone){
+    res.status(422).send({ error: 'Bad input'});
+    return;
+  }
+
+  //Format the phone number to remove the slashes en parens
+  const phone = String(req.body.phone).replace(/[^\d]/g,"");
+
+  //Create a new user account using that phone
+  //Respond to the user request, saying the account was made
+  admin.auth().createUser({ uid : phone })
+    .then(user => res.send(user))
+    .catch(err => res.status(422).send({error : err}))
 }
